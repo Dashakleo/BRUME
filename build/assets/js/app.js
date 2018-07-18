29d127c51e0aea2cfed008f3ba238c4f10084a96
Kolotushkina Daria <dashakleo9@mail.ru>
2018-07-18 18:45:53 +0300

    getting started

---
+++ build/assets/js/app.js
@@ -0,0 +1,11 @@
+// new Glide('.glide').mount()
+// new Glide('.glide', {
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 3
+// }
+
+
+console.log('Hello');
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBuZXcgR2xpZGUoJy5nbGlkZScpLm1vdW50KClcbi8vIG5ldyBHbGlkZSgnLmdsaWRlJywge1xuLy8gICAgIHR5cGU6ICdjYXJvdXNlbCcsXG4vLyAgICAgc3RhcnRBdDogMCxcbi8vICAgICBwZXJWaWV3OiAzXG4vLyB9XG5cblxuY29uc29sZS5sb2coJ0hlbGxvJyk7XG4iXX0=
---
+++ source/js/app.js
@@ -0,0 +1,9 @@
+// new Glide('.glide').mount()
+// new Glide('.glide', {
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 3
+// }
+
+
+console.log('Hello');
